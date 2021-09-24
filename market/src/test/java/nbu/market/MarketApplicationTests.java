2440ef98a19b336c0ccb1c1ad6c8002556905e82
Georgi <georgi_yosifov99@abv.bg>
2021-09-24 17:33:24 +0300

    Intial commit

---
+++ market/src/test/java/nbu/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package nbu.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
