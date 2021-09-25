f39178e7b9b14b6958ed4aa2713b4e850f4978d9
GeorgiYosifov <georgi_yosifov99@abv.bg>
2021-09-26 00:24:34 +0300

    Add changes

--- market/src/main/java/nbu/market/controllers/Market.java
+++ market/src/main/java/nbu/market/controllers/Market.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,50 +19,28 @@ import nbu.market.models.Product;
 @RestController
 @RequestMapping("/api")
 public class Market {
-    
-    @RequestMapping(value="/resource-uri", method=RequestMethod.GET)
-    public SseEmitter handle() 
+
+    private final Product[] Products = new Product[] {
+        new Product("123", "apple", 1.0, "nutritious", 2),
+        new Product("321", "meat", 10.0, "nutritious", 5),
+        new Product("213", "shampoo", 5.0, "non-nutritious", 2)
+    };
+
+    private Product[] Cart = new Product[] {};
+
+    @GetMapping("/priceReducer")
+    public SseEmitter priceReducer() 
     {
         SseEmitter emitter = new SseEmitter();
-        
-        SseEventBuilder eventBuilder = SseEmitter.event();
- 
-        String data = "goshe";
-
-        try {
-            for (int i=0; i < 5; i++) {
-                data += String.valueOf(i);
-                emitter.send(
-                    eventBuilder
-                    .data(data)
-                    .name("dataSet-created")
-                    .id(String.valueOf(data.hashCode()))
-                );
-                Thread.sleep(2000);
-            }
-            
-        } catch (IOException | InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        // Pass the emitter to another component...
-        return emitter;
-    }
-
-    @GetMapping("/emit-data-sets")
-    public SseEmitter fetchData2() 
-    {
-        SseEmitter emitter = new SseEmitter();
-
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
         executor.execute(() -> 
         {
             try {
-                for (int i=0; i < 5; i++) {
+                for(Product p : this.Products) {
                         randomDelay();
-                        emitter.send("Gosheeetuka");
+                        p.Price -= 0.2;
+                        emitter.send(p);
                 }
                 emitter.complete();
             } catch (IOException e) {
@@ -74,18 +53,51 @@ public class Market {
  
     private void randomDelay() {
         try {
-                Thread.sleep(2000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+            Thread.currentThread().interrupt();
         }
     }
 
+    @PostMapping("/product")
+    public void addToCart(Product payload) {
+        //this.Cart
+    }
+
     @GetMapping("/products")
-    public Product[] getAllProducts() { // @RequestParam String text
-        return new Product[] {
-            new Product("123", "apple", 1.0, "nutritious", 2),
-            new Product("321", "meat", 10.0, "nutritious", 5),
-            new Product("213", "shampoo", 5.0, "non-nutritious", 2)
-        };
+    public Product[] getAllProducts() {
+        return this.Products;
     }
 }
+
+ // @RequestParam String text
+ 
+// @RequestMapping(value="/resource-uri", method=RequestMethod.GET)
+    // public SseEmitter handle() 
+    // {
+    //     SseEmitter emitter = new SseEmitter();
+        
+    //     SseEventBuilder eventBuilder = SseEmitter.event();
+ 
+    //     String data = "goshe";
+
+    //     try {
+    //         for (int i=0; i < 5; i++) {
+    //             data += String.valueOf(i);
+    //             emitter.send(
+    //                 eventBuilder
+    //                 .data(data)
+    //                 .name("dataSet-created")
+    //                 .id(String.valueOf(data.hashCode()))
+    //             );
+    //             Thread.sleep(2000);
+    //         }
+            
+    //     } catch (IOException | InterruptedException e) {
+    //         // TODO Auto-generated catch block
+    //         e.printStackTrace();
+    //     }
+
+    //     // Pass the emitter to another component...
+    //     return emitter;
+    // }
