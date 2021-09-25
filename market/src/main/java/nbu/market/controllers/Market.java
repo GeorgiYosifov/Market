ab8767bf0f440d73719baec0666797ed1846a4cd
GeorgiYosifov <georgi_yosifov99@abv.bg>
2021-09-25 23:01:06 +0300

    Add cors

--- market/src/main/java/nbu/market/controllers/Market.java
+++ market/src/main/java/nbu/market/controllers/Market.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEvent
 
 import nbu.market.models.Product;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/api")
 public class Market {
@@ -72,7 +74,7 @@ public class Market {
  
     private void randomDelay() {
         try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
         } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
         }
--- ui/app.js
+++ ui/app.js
@@ -1,48 +1,49 @@
 "use strict";
 
-// const countryData = function (country) {
-//   const data = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-//     .then((response) => {
-//       if (!response.ok) throw new Error(`Test problem `);
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(
-//         `Hello there!The capital of the country is ${data[0].capital}`
-//       );
-//     })
-//     .catch(function (errorObject) {
-//       console.log(errorObject.message);
-//     });
-// };
+const countryData = function(country) {
+    const data = fetch(`http://localhost:8080/api/products`)
+        .then((response) => {
+            if (!response.ok) throw new Error(`Test problem `);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data)
+            console.log(
+                `Hello there!The capital of the country is ${data[0].capital}`
+            );
+        })
+        .catch(function(errorObject) {
+            console.log(errorObject.message);
+        });
+};
 
-// countryData("portugal");
+countryData("portugal");
 
-const itemsList = document.querySelector(".items");
+// const itemsList = document.querySelector(".items");
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://randomuser.me/api/");
-request.send();
+// const request = new XMLHttpRequest();
+// request.open("GET", "http://localhost:8080/api/products");
+// request.send();
 
-request.addEventListener("load", function () {
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-  const html = `
-  <ul class="list-group">
-  <li class="list-group-item"></li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>
+// request.addEventListener("load", function() {
+//     const data = JSON.parse(this.responseText);
+//     console.log(data);
+//     const html = `
+//   <ul class="list-group">
+//   <li class="list-group-item"></li>
+//   <li class="list-group-item">A third item</li>
+//   <li class="list-group-item">A fourth item</li>
+//   <li class="list-group-item">And a fifth one</li>
+// </ul>
 
-<ul class="list-group">
-  <li class="list-group-item">Anem</li>
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>`;
+// <ul class="list-group">
+//   <li class="list-group-item">Anem</li>
+//   <li class="list-group-item">An item</li>
+//   <li class="list-group-item">A second item</li>
+//   <li class="list-group-item">A third item</li>
+//   <li class="list-group-item">A fourth item</li>
+//   <li class="list-group-item">And a fifth one</li>
+// </ul>`;
 
-  itemsList.insertAdjacentHTML("beforeend", html);
-});
+//     itemsList.insertAdjacentHTML("beforeend", html);
+// });
